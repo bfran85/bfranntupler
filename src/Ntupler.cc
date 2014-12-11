9458346899fd67c7e61613f34e6dff4e8f43ea20
bfran85 <brian.a.francisco@gmail.com>
2014-12-11 13:33:17 -0500

    Update Ntupler.cc

--- src/Ntupler.cc
+++ src/Ntupler.cc
@@ -39,9 +39,9 @@
 
 using namespace edm;
 using namespace std;
-                                                        /////////////////////
+/////////////////////
                                                         //class declaration//
-                                                        ////////////////////
+////////////////////
 
 class Ntupler : public edm::EDAnalyzer {
         public:
@@ -50,53 +50,54 @@ class Ntupler : public edm::EDAnalyzer {
 
         private:
 
-                virtual void beginJob() ;
-                virtual void ntuplize(const edm::Event&, const edm::EventSetup&);
-                virtual void endJob() ;
-                int counter;
-                edm::Service<TFileService> fs;  //TFile service for Tree generation
-                TTree *newtree;                 //Pointer for tree
+            virtual void beginJob() ;
+            virtual void analyze(const edm::Event&, const edm::EventSetup&);
+            virtual void endJob() ;
+            int counter;
+            edm::Service<TFileService> fs;  //TFile service for Tree generation
+            TTree *newtree;                 //Pointer for tree
 
-                int nJets_AK5-PF;
-                int nJets_AK7-PF;
-                vector<float> ak5jet_px;
-                vector<float> ak5jet_py;
-                vector<float> ak5jet_pz;
-                vector<float> ak5jet_e;
-                vector<float> ak5jet_eta;
-                vector<float> ak5jet_phi;
-                vector<float> ak5jet_pt;
-                vector<float> ak7jet_px;
-                vector<float> ak7jet_py;
-                vector<float> ak7jet_pz;
-                vector<float> ak7jet_e;
-                vector<float> ak7jet_eta;
-                vector<float> ak7jet_phi;
-                vector<float> ak5jet_pt;
-                vector<float> ak5jet_CSVbdisc;
-                vector<float> ak7jet_CSVbdisc;
+            int nJets_AK5PF;
+            int nJets_AK7PF;
+            vector<float> ak5jet_px;
+            vector<float> ak5jet_py;
+            vector<float> ak5jet_pz;
+            vector<float> ak5jet_e;
+            vector<float> ak5jet_eta;
+            vector<float> ak5jet_phi;
+            vector<float> ak5jet_pt;
+            vector<float> ak7jet_px;
+            vector<float> ak7jet_py;
+            vector<float> ak7jet_pz;
+            vector<float> ak7jet_e;
+            vector<float> ak7jet_eta;
+            vector<float> ak7jet_phi;
+            vector<float> ak7jet_pt;
+            vector<float> ak5jet_CSVbdisc;
+            vector<float> ak7jet_CSVbdisc;
 
-        // ----------member data ---------------------------
-        edm::InputTag src_;
-        edm::InputTag src2_;
-        edm::InputTag srcAK7_;
-        edm::InputTag srcAK5_;
+            // ----------member data ---------------------------
+            edm::InputTag src_;
+            edm::InputTag src2_;
+            edm::InputTag srcAK7_;
+            edm::InputTag srcAK5_;
+};
 
 
 /// constructors and destructor
 ///
-Ntupler::Ntupler(const edm::ParameterSet& iConfig) {
+Ntupler::Ntupler(const edm::ParameterSet& iConfig) :
         src_( iConfig.getParameter<edm::InputTag>( "src" ) ),                   // Obtain input
-         src2_( iConfig.getParameter<edm::InputTag>( "srcGen" ) )                        // Obtain input
+        src2_( iConfig.getParameter<edm::InputTag>( "srcGen" ) ),                        // Obtain input
         srcAK7_( iConfig.getParameter<edm::InputTag>("srcAK7") ),
-        srcAK5_( iConfig.getParameter<edm::InputTag>("srcAK5") )
+        srcAK5_( iConfig.getParameter<edm::InputTag>("srcAK5") ) {
 
         // Declare new tree
         newtree = fs->make<TTree>("DataSetTree","Analysis Tree for Data Set");
 
         // Create branches for new tree
-//      newtree->Branch("nJets_AK5-PF",&nJets_AK5-PF,"nJets_AK5-PF/I");
-//      newtree->Branch("nJets_AK7-PF",&nJets_AK7-PF,"nJets_AK7-PF/I");
+// newtree->Branch("nJets_AK5-PF",&nJets_AK5-PF,"nJets_AK5-PF/I");
+// newtree->Branch("nJets_AK7-PF",&nJets_AK7-PF,"nJets_AK7-PF/I");
         newtree->Branch("ak5jet_px",&ak5jet_px);
         newtree->Branch("ak5jet_py",&ak5jet_py);
         newtree->Branch("ak5jet_pz",&ak5jet_pz);
@@ -113,8 +114,8 @@ Ntupler::Ntupler(const edm::ParameterSet& iConfig) {
         newtree->Branch("ak7jet_pt",&ak7jet_pt);
 //      newtree->Branch("ak5jet_CSVbdisc",&ak5jet_CSVbdisc);
 //      newtree->Branch("ak7jet_CSVbdisc",&ak7jet_CSVbdisc);
-//      newtree->Branch("ak5jet_DINKObdisc[nJets_AK5-PF]",&ak5jet_DINKObdisc);
-//      newtree->Branch("ak7jet_DINKObdisc[nJets_AK7-PF]",&ak7jet_DINKObdisc);
+// newtree->Branch("ak5jet_DINKObdisc[nJets_AK5-PF]",&ak5jet_DINKObdisc);
+// newtree->Branch("ak7jet_DINKObdisc[nJets_AK7-PF]",&ak7jet_DINKObdisc);
 
 }
 
@@ -146,36 +147,35 @@ Ntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
         // Way to call ak5PFJETS
         edm::Handle<std::vector<reco::PFJet>> jet5;
         iEvent.getByLabel(srcAK5_ , jet5);
-        const std::vector<reco::PFJet> & j5 = *jet5;     // AODSIM
 
         // Way to call ak7PFJETS
         edm::Handle<std::vector<reco::PFJet>> jet7;
         iEvent.getByLabel(srcAK7_ , jet7);
-        const std::vector<reco::PFJet> & j7 = *jet7;
 
-        for (int iJet = 0; iJet < (int)jets->size(); iJet++)
+        for (int iJet = 0; iJet < (int)jet5->size(); iJet++)
         {   reco::PFJet j5 = jet5->at(iJet);
-            reco::PFJet j7 = jet7->at(iJet);
-
-            ak5jet_px.push_back( jet5.px() );
-            ak5jet_py.push_back( jet5.py() );
-            ak5jet_pz.push_back( jet5.pz() );
-            ak5jet_e.push_back( jet5.e() );
-            ak5jet_eta.push_back( jet5.eta() );
-            ak5jet_phi.push_back( jet5.phi() );
-            ak5jet_pt.push_back( jet5.pt() );
-
-            ak7jet_px.push_back( jet7.px() );
-            ak7jet_py.push_back( jet7.py() );
-            ak7jet_pz.push_back( jet7.pz() );
-            ak7jet_e.push_back( jet7.e() );
-            ak7jet_eta.push_back( jet7.eta() );
-            ak7jet_phi.push_back( jet7.phi() );
-            ak7jet_pt.push_back( jet7.pt() );
-
-
+            ak5jet_px.push_back( j5.px() );
+            ak5jet_py.push_back( j5.py() );
+            ak5jet_pz.push_back( j5.pz() );
+            ak5jet_e.push_back( j5.e() );
+            ak5jet_eta.push_back( j5.eta() );
+            ak5jet_phi.push_back( j5.phi() );
+            ak5jet_pt.push_back( j5.pt() );
         }
 
+        for (int iJet = 0; iJet < (int)jet7->size(); iJet++)
+        {   reco::PFJet j7 = jet7->at(iJet); 
+            ak7jet_px.push_back( j7.px() );
+            ak7jet_py.push_back( j7.py() );
+            ak7jet_pz.push_back( j7.pz() );
+            ak7jet_e.push_back( j7.e() );
+            ak7jet_eta.push_back( j7.eta() );
+            ak7jet_phi.push_back( j7.phi() );
+            ak7jet_pt.push_back( j7.pt() );
+        }
+
+        nJets_AK5PF = (int)jet5->size();
+        nJets_AK7PF = (int)jet7->size();
         newtree->Fill();
 
         // clear the vectors for next event
@@ -207,3 +207,5 @@ void
 Ntupler::endJob()
 {
 }
+
+DEFINE_FWK_MODULE(Ntupler);

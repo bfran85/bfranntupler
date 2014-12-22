c832273a02370cdc225d44783cf10373076d0d2b
bfran85 <brian.a.francisco@gmail.com>
2014-12-22 09:22:53 -0500

    Update Ntupler.cc

--- src/Ntupler.cc
+++ src/Ntupler.cc
@@ -100,8 +100,8 @@ Ntupler::Ntupler(const edm::ParameterSet& iConfig) :
         newtree = fs->make<TTree>("DataSetTree","Analysis Tree for Data Set");
 
         // Create branches for new tree
-//      newtree->Branch("nJets_AK5-PF",&nJets_AK5-PF,"nJets_AK5-PF/I");
-//      newtree->Branch("nJets_AK7-PF",&nJets_AK7-PF,"nJets_AK7-PF/I");
+        newtree->Branch("nJets_AK5-PF",&nJets_AK5-PF,"nJets_AK5-PF/I");
+        newtree->Branch("nJets_AK7-PF",&nJets_AK7-PF,"nJets_AK7-PF/I");
         newtree->Branch("ak5jet_px",&ak5jet_px);
         newtree->Branch("ak5jet_py",&ak5jet_py);
         newtree->Branch("ak5jet_pz",&ak5jet_pz);
@@ -175,7 +175,7 @@ Ntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
             ak5jet_eta.push_back( j5.eta() );
             ak5jet_phi.push_back( j5.phi() );
             ak5jet_pt.push_back( j5.pt() );
-//          ak5jet_CSVbdisc.push_back( bTags[j5].second );  <-- Needs to correspond to this particular jet
+            ak5jet_CSVbdisc.push_back( bTags[j5.index].second );  //<-- Needs to correspond to this particular jet
         }
 
         for (int iJet = 0; iJet < (int)jet7->size(); iJet++)
@@ -187,7 +187,7 @@ Ntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
             ak7jet_eta.push_back( j7.eta() );
             ak7jet_phi.push_back( j7.phi() );
             ak7jet_pt.push_back( j7.pt() );
-//          ak7jet_CSVbdisc.push_back( bTags[j7].second );  <-- Needs to correspond to this particular jet
+            ak7jet_CSVbdisc.push_back( bTags[j7.index].second );  //<-- Needs to correspond to this particular jet
         }
 
         nJets_AK5PF = jet5->size();
@@ -202,7 +202,7 @@ Ntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
         ak5jet_eta.clear();
         ak5jet_phi.clear();
         ak5jet_pt.clear();
-//      ak5jet_CSVbdisc.clear();
+        ak5jet_CSVbdisc.clear();
 
         ak7jet_px.clear();
         ak7jet_py.clear();
@@ -211,7 +211,7 @@ Ntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
         ak7jet_eta.clear();
         ak7jet_phi.clear();
         ak7jet_pt.clear();
-//      ak7jet_CSVbdisc.clear();
+        ak7jet_CSVbdisc.clear();
 
 }
 
